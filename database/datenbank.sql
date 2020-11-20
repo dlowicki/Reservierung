a0857b4c5f188661632ff96325e2f8b22e92d001
dlowicki <44500521+dlowicki@users.noreply.github.com>
2020-11-20 13:42:26 +0100

    Datenbank update

--- database/datenbank.sql
+++ database/datenbank.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 21. Okt 2020 um 14:25
--- Server-Version: 10.4.11-MariaDB
--- PHP-Version: 7.3.18
+-- Erstellungszeit: 20. Nov 2020 um 13:40
+-- Server-Version: 10.4.14-MariaDB
+-- PHP-Version: 7.3.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -57,7 +57,15 @@ INSERT INTO `rclient` (`clientID`, `reserveID`, `clientVorname`, `clientName`, `
 ('5f8d41d624334', 85, 'David', 'Lowicki', 'flowicki@web.de', 'Blücherstraße 17a', 'wasdwasd', '2020-10-19 09:35:50', '5f8d41d62a53e'),
 ('5f8d5f9815ea4', 86, 'David', 'Lowicki', 'mertero@web.de', 'Blücherstraße 17a', 'waswwasd', '2020-10-19 11:42:48', '5f8d5f981c4b5'),
 ('5f8d61ae8069c', 89, 'David', 'König', 'koenig@mail.com', 'koenigadresse 2', 'telefonnummer', '2020-10-19 11:51:42', '5f8d61ae87589'),
-('5f8ee671d5ea4', 85, 'David2', 'David2', 'David2@web.de', 'David2Straße', 'David2', '2020-10-20 15:30:25', '5f8ee671d33a8');
+('5f8ee671d5ea4', 85, 'David2', 'David2', 'David2@web.de', 'David2Straße', 'David2', '2020-10-20 15:30:25', '5f8ee671d33a8'),
+('5f917476db540', 93, 'David', 'Lowicki', 'lowicki@gmx.de', 'lowickistraße 2', '914832479324', '2020-10-22 14:00:54', '5f917476e2755'),
+('5f91820f4a1c2', 99, 'David', 'Lowicki', 'lowicki@gmx.de', 'lowickistraße 2', '914832479324', '2020-10-22 14:58:55', '5f91820f4ed26'),
+('5f91820f4f507', 99, 'Test22', 'Test22', 'Test22', 'Test22', 'Test22', '2020-10-22 14:58:55', '5f91820f4f51c'),
+('5f97d70f0670d', 100, 'David', 'Lowicki', 'Test@web.de', 'TestAdresse', 'TestNummer', '2020-10-27 09:15:11', '5f97d70f0a9b2'),
+('5fabc6e5bce0d', 101, 'test', 'test', 'test', 'test', 'test', '2020-11-11 12:11:33', '5fabc6e5c09f65fabc6e5c09f8'),
+('5fabdd3fb396c', 102, 'test1', 'test1', 'test1', 'test1', 'test1', '2020-11-11 13:46:55', '5fabdd3fb75915fabdd3fb7594'),
+('5fabdd3fb79f8', 102, 'test', 'test', 'test', 'test', 'test', '2020-11-11 13:46:55', '5fabdd3fb7a025fabdd3fb7a03'),
+('f93b6ec4aaec', 100, 'Luca', 'Yavsan', '', '', '', '2020-10-28 14:51:59', '5f99777f321f8');
 
 -- --------------------------------------------------------
 
@@ -67,8 +75,6 @@ INSERT INTO `rclient` (`clientID`, `reserveID`, `clientVorname`, `clientName`, `
 
 CREATE TABLE `rnoshow` (
   `nsID` int(11) NOT NULL,
-  `clientID` varchar(35) COLLATE utf8mb4_german2_ci NOT NULL,
-  `reserveID` int(11) NOT NULL,
   `nsMail` varchar(50) COLLATE utf8mb4_german2_ci NOT NULL,
   `nsAmount` int(11) NOT NULL,
   `nsDate` datetime NOT NULL
@@ -78,8 +84,11 @@ CREATE TABLE `rnoshow` (
 -- Daten für Tabelle `rnoshow`
 --
 
-INSERT INTO `rnoshow` (`nsID`, `clientID`, `reserveID`, `nsMail`, `nsAmount`, `nsDate`) VALUES
-(4, '5f8d41d624334', 85, 'flowicki@web.de', 2, '2020-10-19 11:00:26');
+INSERT INTO `rnoshow` (`nsID`, `nsMail`, `nsAmount`, `nsDate`) VALUES
+(4, 'flowicki@web.de', 7, '2020-10-19 11:00:26'),
+(14, 'test@web.de', 1, '2020-11-17 09:06:49'),
+(17, 'test@domain.de', 1, '2020-11-17 09:43:23'),
+(21, 'wasd@domain.de', 3, '2020-11-17 12:31:46');
 
 -- --------------------------------------------------------
 
@@ -105,12 +114,6 @@ CREATE TABLE `rreserve` (
 --
 
 INSERT INTO `rreserve` (`reserveID`, `tableID`, `clientID`, `reserveDate`, `reserveStart`, `reserveEnd`, `reserveDuration`, `reserveAmount`, `reserveCookie`, `reserveState`) VALUES
-(23, 43, '5f3d230fc603e', '0000-00-00', '17:03:00', '00:00:00', '2:30', 10, '', 0),
-(24, 26, '5f3d22b5a9d98', '0000-00-00', '18:01:00', '00:00:00', '2:30', 10, '', 0),
-(25, 43, '5f3d230fc603e', '0000-00-00', '20:03:00', '00:00:00', 'gz', 10, '', 0),
-(26, 10, '5f3d23a8bef27', '0000-00-00', '18:05:00', '00:00:00', '2:30', 10, '', 0),
-(27, 33, '5f3d26bb180a6', '0000-00-00', '18:18:00', '00:00:00', '2:30', 10, '', 0),
-(28, 33, '5f3d2763a06ce', '0000-00-00', '20:49:00', '00:00:00', '2:30', 10, '', 0),
 (29, 8, '5f3e2b1a6f4bc', '0000-00-00', '21:08:00', '00:00:00', '2:30', 10, '', 0),
 (30, 31, '5f3e59e1b33f8', '0000-00-00', '17:09:00', '00:00:00', '2:30', 10, '', 0),
 (32, 8, '5f4379bb46fe7', '0000-00-00', '17:26:00', '00:00:00', 'gz', 10, '', 0),
@@ -122,12 +125,17 @@ INSERT INTO `rreserve` (`reserveID`, `tableID`, `clientID`, `reserveDate`, `rese
 (46, 9, '5f461494b7dbd', '0000-00-00', '18:55:00', '00:00:00', 'gz', 5, '', 0),
 (51, 9, '5f46184a6e791', '0000-00-00', '14:05:00', '00:00:00', '2:30', 15, '', 0),
 (53, 98, '5f4746a85235e', '0000-00-00', '17:00:00', '00:00:00', '2:30', 10, '', 0),
-(73, 34, '5f869ad0ef8e7', '0000-00-00', '20:00:00', '00:00:00', '2:30', 5, '5f869ad0ef8e9', 4),
+(73, 34, '5f869ad0ef8e7', '2020-11-11', '20:00:00', '11:12:00', '2:30', 5, '5f869ad0ef8e9', 4),
 (76, 34, '5f86a4310f504', '0000-00-00', '17:00:00', '00:00:00', '2:30', 5, '5f86a4310f507', 1),
 (78, 8, '5f86e1683a338', '0000-00-00', '17:00:00', '03:07:00', 'gz', 1, '5f86e1683a33b', 0),
 (79, 35, '5f86f17285e83', '0000-00-00', '17:00:00', '03:00:00', '2:30', 1, '5f86f17285e86', 0),
 (85, 34, '5f8d41d624334', '2020-10-20', '18:30:00', '21:00:00', '2:30', 3, '5f8d41d624339', 1),
-(92, 32, '5f8d6295e7c7c', '2020-10-20', '17:00:00', '19:30:00', '2:30', 1, '5f8d6295e7c7e', 0);
+(92, 32, '5f8d6295e7c7c', '2020-10-20', '17:00:00', '19:30:00', '2:30', 1, '5f8d6295e7c7e', 0),
+(93, 34, '5f917476db540', '2020-10-22', '17:00:00', '19:30:00', '2:30', 5, '5f917476db543', 0),
+(99, 34, '5f91820f4a1c2', '2020-10-22', '22:00:00', '00:00:00', '2:30', 5, '5f91820f4a1c6', 0),
+(100, 34, '5f97d70f0670d', '2020-10-27', '17:00:00', '19:30:00', '2:30', 5, '5f97d70f06712', 1),
+(101, 9, '5fabc6e5bce0d', '2020-11-11', '17:00:00', '22:00:00', 'gz', 5, '5fabc6e5bce11', 0),
+(102, 34, '5fabdd3fb396c', '2020-11-11', '17:00:00', '19:30:00', '2:30', 3, '5fabdd3fb3970', 0);
 
 -- --------------------------------------------------------
 
@@ -169,7 +177,7 @@ INSERT INTO `rtable` (`tableID`, `tableType`, `tableMax`, `tableMin`, `tableCode
 (33, 'one', 4, 1, 'b6EsC24FskeVaKF6Nwm6PA', 'open'),
 (34, 'left', 8, 5, 'zpJ09e69sECQodzWlnGacQ', 'open'),
 (35, 'one', 4, 1, '9Q8MbJT300a4u51lI1HCWg', 'open'),
-(36, 'left', 7, 5, 'GOwXG6bYrUyefLddYt3uhg', 'open'),
+(36, 'right-bottom', 7, 5, 'GOwXG6bYrUyefLddYt3uhg', 'open'),
 (37, 'right', 7, 5, 'PnYzx4dNHEiRJeTE6zye8A', 'open'),
 (41, 'pillar', 4, 1, '9MBPz9kHvUiurUfC1fIeqg', 'open'),
 (42, 'pillar', 6, 1, 'Aifs0HuKHEyYP7YiUc6Msw', 'open'),
@@ -187,7 +195,7 @@ INSERT INTO `rtable` (`tableID`, `tableType`, `tableMax`, `tableMin`, `tableCode
 (54, 'big', 4, 3, 'lQXh61LY70aWeeS6G0DHfA', 'open'),
 (55, 'big', 5, 3, 'Svu97xQrrkOMllNdQFeBcg', 'open'),
 (56, 'big', 5, 3, 'lyQTRwARyUqNCcMIDerMlA', 'open'),
-(57, 'right', 7, 5, '8Zp8QKBzn0mvkHPsIAEaSA', 'open'),
+(57, 'right-layed', 7, 5, '8Zp8QKBzn0mvkHPsIAEaSA', 'open'),
 (98, 'pillar', 10, 1, 'yJGSumzsqEGLainM1jCRJw', 'open'),
 (99, 'right', 10, 5, 'rRNLXwxp9Ea8c4WOkl5p9w', 'open');
 
@@ -212,7 +220,7 @@ CREATE TABLE `ruser` (
 --
 
 INSERT INTO `ruser` (`userID`, `userName`, `userPW`, `userIP`, `userActive`, `userPermission`, `userCookie`) VALUES
-(1, 'b32cb8284e95d0b29dc79c649a10d20c', 'b32cb8284e95d0b29dc79c649a10d20c', '', 1, '1', '5a12fdae0b8fa1569043e6476a30fb3c');
+(1, 'b32cb8284e95d0b29dc79c649a10d20c', 'b32cb8284e95d0b29dc79c649a10d20c', '', 1, '1', '060e7e6c5b06280165871226dc406e03');
 
 --
 -- Indizes der exportierten Tabellen
@@ -257,13 +265,13 @@ ALTER TABLE `ruser`
 -- AUTO_INCREMENT für Tabelle `rnoshow`
 --
 ALTER TABLE `rnoshow`
-  MODIFY `nsID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `nsID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT für Tabelle `rreserve`
 --
 ALTER TABLE `rreserve`
-  MODIFY `reserveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+  MODIFY `reserveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 
 --
 -- AUTO_INCREMENT für Tabelle `ruser`
